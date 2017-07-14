d55e04d1fead6dc85e70ed68e3c85aaf4bab762b
esosdiaz <diazgonzalez.perla@gmail.com>
2017-07-14 13:56:04 -0300

    html y estilos seccion saldo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 /* wabalaba dub dub */
 $(document).ready(function(){
-	$("#open-hide").click(function(){
+	$("#open-hide").click(function(){//esconde y muestra el menu lateral
 		$(this).toggleClass("show");
 	});
+
+	var estado=false;
+	$("#btn-toggle").on('click',function(){
+				$('.seccion-toggle').slideToggle();
+			});
+	
 });
